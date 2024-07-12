282f4fef0a66d231f77efd3237f77c7ed528008b
Ryan <141914774+rndmzd@users.noreply.github.com>
2024-07-12 10:08:14 -0500

    Add controller and sensor files.

---
+++ controller/esp32_solenoid_controller_http/esp32_solenoid_controller_http.ino
@@ -0,0 +1,122 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+#include <ArduinoOTA.h>
+#include "config.h"
+
+const char* wifi_ssid = WIFI_SSID;
+const char* wifi_password = WIFI_PASSWORD;
+
+WebServer server(80);
+
+const int solenoidPin = 2;
+
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+    server.send(400, "text/plain", "Body not received");
+    return;
+  }
+
+  String body = server.arg("plain");
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, body);
+
+  if (error) {
+    server.send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+
+  bool solenoidState = doc["solenoidState"];
+  digitalWrite(solenoidPin, solenoidState ? HIGH : LOW);
+
+  server.send(200, "application/json", "{\"status\":\"success\"}");
+}
+
+void reconnectWiFi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.begin(wifi_ssid, wifi_password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println("Reconnected to WiFi");
+  }
+}
+
+void setupOTA() {
+  // Hostname defaults to esp32-[MAC]
+  ArduinoOTA.setHostname("esp32-solenoid-controller");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(solenoidPin, OUTPUT);
+  digitalWrite(solenoidPin, LOW);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("connected.");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/control", HTTP_POST, handlePost);
+
+  server.begin();
+  Serial.println("Server started");
+
+  setupOTA();
+}
+
+void loop() {
+  // Handle client requests
+  server.handleClient();
+
+  ArduinoOTA.handle();
+
+  reconnectWiFi();
+}
---
+++ sensor/main.py
@@ -0,0 +1,64 @@
+import network
+import urequests
+import machine
+import time
+import config
+
+WATERING_THRESHOLD = 50
+
+solenoid_controller_ip = config.SOLENOID_CONTROLLER_IP
+
+wifi_ssid = config.WIFI_SSID
+wifi_password = config.WIFI_PASSWORD
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+def connect_to_wifi():
+    if not wlan.isconnected():
+        print("Connecting to WiFi...")
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            time.sleep(1)
+            print(".")
+        print("Connected to WiFi")
+
+connect_to_wifi()
+
+# Define the analog pins for the sensors
+sensor1 = machine.ADC(26)
+sensor2 = machine.ADC(27)
+
+def read_sensors():
+    value1 = sensor1.read_u16()
+    value2 = sensor2.read_u16()
+    return value1, value2
+
+def scale_value(raw_value):
+    return int((raw_value / 65535) * 100)
+
+def send_data(value1, value2):
+    scaled_value1 = scale_value(value1)
+    print("scaled_value1:", scaled_value1)
+    scaled_value2 = scale_value(value2)
+    print("scaled_value2:", scaled_value2)
+    
+    url = 'http://' + solenoid_controller_ip + '/control'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'sensor1': scaled_value1,
+        'sensor2': scaled_value2,
+        'solenoidState': scaled_value1 > WATERING_THRESHOLD
+    }
+    try:
+        response = urequests.post(url, json=data, headers=headers)
+        print(response.text)
+        response.close()
+    except:
+        print("Failed to send data.")
+
+while True:
+    connect_to_wifi()
+    value1, value2 = read_sensors()
+    send_data(value1, value2)
+    time.sleep(10)
