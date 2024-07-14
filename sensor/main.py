cf71ae6989b25eb78b396760f59f6b20e3da51b8
Ryan <141914774+rndmzd@users.noreply.github.com>
2024-07-13 22:32:38 -0500

    Clean-up sensor script.

--- sensor/main.py
+++ sensor/main.py
@@ -4,8 +4,14 @@ import machine
 import time
 import config
 
-WATERING_THRESHOLD = 50
+led = machine.Pin("LED", machine.Pin.OUT)
+led.off()
 
+sensor1 = machine.ADC(26)
+sensor2 = machine.ADC(27)
+
+watering_threshold = config.WATERING_THRESHOLD
+data_send_interval = config.DATA_SEND_INTERVAL
 solenoid_controller_ip = config.SOLENOID_CONTROLLER_IP
 
 wifi_ssid = config.WIFI_SSID
@@ -23,12 +29,6 @@ def connect_to_wifi():
             print(".")
         print("Connected to WiFi")
 
-connect_to_wifi()
-
-# Define the analog pins for the sensors
-sensor1 = machine.ADC(26)
-sensor2 = machine.ADC(27)
-
 def read_sensors():
     value1 = sensor1.read_u16()
     value2 = sensor2.read_u16()
@@ -38,6 +38,8 @@ def scale_value(raw_value):
     return int((raw_value / 65535) * 100)
 
 def send_data(value1, value2):
+    led.on()
+    
     scaled_value1 = scale_value(value1)
     print("scaled_value1:", scaled_value1)
     scaled_value2 = scale_value(value2)
@@ -48,7 +50,8 @@ def send_data(value1, value2):
     data = {
         'sensor1': scaled_value1,
         'sensor2': scaled_value2,
-        'solenoidState': scaled_value1 > WATERING_THRESHOLD
+        #'solenoidState': scaled_value1 > watering_threshold,
+        'solenoidState': False
     }
     try:
         response = urequests.post(url, json=data, headers=headers)
@@ -56,9 +59,11 @@ def send_data(value1, value2):
         response.close()
     except:
         print("Failed to send data.")
+    
+    led.off()
 
 while True:
     connect_to_wifi()
     value1, value2 = read_sensors()
     send_data(value1, value2)
-    time.sleep(10)
+    time.sleep(data_send_interval)
